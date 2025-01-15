c9fb98b7c37518c0209ee43a024ac9e295c039fd
Adenekan Mutiat <lamidemutiat@gmail.com>
2025-01-15 16:30:13 +0100

    Updated website with new changes

---
+++ ML logistics/javascript/script.js
@@ -0,0 +1,17 @@
+/*var num= 5;
+var num1 = 10;
+var multiply = num * num1;
+var subtraction = num1 - num;
+
+
+ 
+/*for(x=0; x<=10; x++){
+    document.write(x + " I am learning programming in Mackintouch Consulting<br>");
+
+
+let x =0;
+while(x<=10){
+    document.write(x + " My name is Mutiat <br>");
+    x++;
+}
+*/
